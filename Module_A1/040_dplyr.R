bf2c111b240807700df93d0584e786f5011a0f38
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-03-03 17:22:31 +0100

    half through with data manipulation (dplyr) chapter.

--- Module_A1/040_dplyr.R
+++ Module_A1/040_dplyr.R
@@ -9,38 +9,193 @@
 # Datenmanipulation (using dplyr)
 library(dplyr)
 
-# Keyboard shortcut for %>%:
-# Cmd + Shift + M
+# load iris dataset
+data(iris)
 
-iris <- as_tibble(iris)
-iris
+# inspect data
+head(iris)
+str(iris)
+summary(iris)
+
 class(iris)
 
-# slice() - choose rows
+# make 'iris' a 'tibble
+iris <- as_tibble(iris)
+class(iris)
+print(iris)
+
+
+
+##########################  %>%  - the Pipe Operator ##########################
+# Keyboard shortcut for Pipe '%>%':
+# Cmd + Shift + M
+iris %>% print
+
+
+
+############################### slice() - choose Rows #########################
+
+# first row
 iris %>% slice(1)
 
+# last row
+iris %>% slice(n())                             # n() returns number of rows
 
-# filter() - only rows matching conditions
+# with Pipe (%>%)
+iris %>% slice(1)
+iris %>% slice(n())
+
+# without Pipe (%>%)
+slice(iris, 1)
+slice(iris, n())
+
+# choose multiple rows
+iris %>% slice(c(1, 4, 10, 15, n()))
+
+
+
+##############  filter() - choose Rows matching certain conditions ############
+
+# filter by two conditions
 iris %>% filter(Species == "setosa" & Petal.Width > 0.3)
 
 
 
-# select() - columns / variables
+################ arrange() - sort Data by one or more Columns #################
 
+# sort by one Columns
+iris %>% arrange(Sepal.Length)
 
-# arrange() - sort
+# descendant by Species, then ascendant by Petal Width and Length
 iris %>% arrange(desc(Species), Petal.Width, Petal.Length)
 
 
-# mutate() - create new variables / columns
+
+####################### select() - only certain Columns #######################
+
+# select two Columns
+iris %>% select(Sepal.Length, Species)
+
+# select a sequence of Columns
+iris %>% select(Petal.Length:Species)
+
+# select all Columns except the ones with leading minus (-)
+iris %>% select(-Petal.Width, -Sepal.Length)
+
+# functions to use within select():
+#       starts_with()
+#       ends_with()
+#       contains()
+#       matches()
+#       num_range()
+
+# select all Columns beginning with 'Petal'
+iris %>% select(starts_with("Petal"))
+
+# select all Columns containing 'width'
+iris %>% select(contains("width"))
+
+# select all Columns NOT containing 'width'
+iris %>% select(-contains("width"))
 
 
-# group_by() - group data
+
+######################## rename() - rename Columns ########################
+
+# rename a Column
+iris %>% rename(Species2=Species)
+
+# select() can also be used
+# but returns only the specified Columns
+iris %>% select(Species2=Species, Petal.Width)
 
 
-# sumarize() - calculate statistics
-# 
 
+####################### distinct() - filters unique Rows ######################
+
+# 150 Rows
+iris %>% dim
+
+# but only 149 distinct Rows
+iris %>% distinct %>% dim
+
+# distinct Values of a Variable
+iris %>% distinct(Species)
+
+
+
+######################## mutate() - add new Columns ###########################
+
+# add logical Column 'is_sertosa'
+iris %>% mutate(is_setosa = Species == "setosa")
+
+# case_when()
+# add Column 'sepal2' with value short/long depending on Sepal.Length
+iris %>%
+  mutate(sepal2 = case_when(Sepal.Length < 5 ~ "short",
+                            Sepal.Length >= 5 ~ "long")) %>%
+  select(Species, Sepal.Length, sepal2)
+
+# ifelse()
+iris %>%
+  mutate(sepal2 = ifelse(Sepal.Length < 5, "short", "long")) %>% 
+  select(Species, Sepal.Length, sepal2)
+
+######################## transmute() - only new Columns #######################
+
+# transmute returns only specified Columns
+# here: is_setosa, Species
+iris %>% transmute(is_setosa = Species == "setosa", Species)
+
+# newly created Columns can be used in the same statement
+iris %>%
+  select(starts_with("Sepal"), Species) %>%
+  arrange(Sepal.Width) %>%
+  mutate(logicalV = Species == "setosa", numV = as.numeric(logicalV))
+
+  
+
+########################### group_by() - group Data ###########################
+
+# group by 'Species'
+# summarise: - count for each Species
+#            - min/max Sepal.Length
+iris %>%
+  group_by(Species) %>%
+  summarise(
+    count = n(),
+    min_Sepal.Length = min(Sepal.Length),
+    max_Sepal.Length = max(Sepal.Length)
+  )
+
+# group by and show first two records
+iris %>%
+  group_by(Species) %>%
+  slice(1:2)
+
+###################### sumarize() - calculate statistics ######################
+
+# summarize_all()
+# summarize 'mean' of all Columns
+# returns NA for Species (not numeric/logical)
+iris %>%
+  group_by(Species) %>% 
+  summarize_all(mean)
+
+# summarize_at()
+# iris %>% 
+#   group_by(Species) %>% 
+#   summarize_at(Species, mean)
+
+
+# summarize_if()
+iris %>%
+  summarize_if(is.numeric, mean)
+
+iris %>%
+  group_by(Species) %>%
+  summarize_if(is.numeric, list(mean, median)) %>%
+  select(starts_with("Sepal"))
 
 
 # Exercises "datenmanipulation_beispiel"
