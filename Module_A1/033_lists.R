28300c2194eef45fb3c557fc80d2c1bec0a1af3c
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-03-03 15:12:52 +0100

    data manipulation chapter 4 started.

--- Module_A1/033_lists.R
+++ Module_A1/033_lists.R
@@ -18,8 +18,8 @@ str(lst)
 
 # access list elements
 class(lst[2])       # CAVE: returns a list!
-class(lst[[2]])     # correct data type
-class(lst$df)       # correct data type
+class(lst[[2]])     # returns elements data type
+class(lst$df)       # returns elements data type
 
 lm1 <- lm(Price ~ Weight, data = Cars93)
 lm1
@@ -28,7 +28,6 @@ lm1$coefficients
 
 ############################# Exercises: Lists #############################
 
-# 5.
 # 5a. Erzeugen Sie eine Liste ll mit insgesamt 3 Listenelementen:
 #        v1: numerischer Vektor der Länge 10
 #        v2: character Vektor der Länge 15
@@ -36,70 +35,36 @@ lm1$coefficients
 ll <- list(v1 = sample(10),
            v2 = letters[1:15],
            v3 = sample(c(TRUE, FALSE), size = 5, replace = TRUE))
+
 ll$v1
 class(ll$v1)
+
 ll$v2
 class(ll$v2)
+
 ll$v3
 class(ll$v3)
+
 
 # 5b. Extrahieren Sie aus ll das Element v2 und geben Sie das
 #    Ergebnis als Liste zurück.
 ll["v2"]
 class(ll["v2"])
 
+
 # 5c. Extrahieren Sie aus ll das Element v3 und geben Sie das
 #    Ergebnis als Vektor zurück.
 ll$v3
 class(ll$v3)
+# or:
+ll[["v3"]]
+class(ll[["v3"]])
+
 
 # 5d. Extrahieren Sie aus ll die ersten drei Elemente von v1
 #    und geben Sie das Ergebnis als Vektor zurück.
 ll$v1[1:3]
 ll[["v1"]][1:3]
 
-# Geben Sie die Namen der Listenelemente zurück.
-names(ll)
-
-
-
-
-
-# 5a. Erzeugen Sie eine Liste ll mit insgesamt 3 Listenelementen:
-#     v1: numerischer Vektor der Länge 10
-#     v2: character Vektor der Länge 15
-#     v3: logischer Vektor der Länge 5
-ll <- list(
-  v1 = 1:10,
-  v2 = LETTERS[1:15],
-  v3 = sample(c(TRUE, FALSE), size = 5, replace = TRUE)
-)
-
-ll
-
-
-# 5b. Extrahieren Sie aus ll das Element v2 und geben Sie das Ergebnis als
-#     Liste zurück.
-l2 <- ll[2]
-l2
-class(l2)
-
-
-# 5c. Extrahieren Sie aus ll das Element v3 und geben Sie das Ergebnis als
-#     Vektor zurück.
-l3 <- ll[[3]]
-l3
-class(l3)
-# or:
-l3 <- ll$v3
-l3
-class(l3)
-
-
-# 5d. Extrahieren Sie aus ll die ersten drei Elemente von v1 und geben Sie
-#     das Ergebnis als Vektor zurück.
-ll$v1[1:3]
-# or:
-ll[[1]][1:3]
 # Geben Sie die Namen der Listenelemente zurück.
 names(ll)
--- Module_A1/034_functions.R
+++ Module_A1/034_functions.R
@@ -4,13 +4,59 @@
 #   datentypen.pdf
 #   datentypen_beispiel.pdf
 
+################################# NA ################################
+# NAs are often the result of data conversions
+v1 <- c(1:2, "a", 3:4, "b")
+v1
+
+v2 <- as.integer(v1)
+v2
+
+# is.na() detects NAs
+is.na(v2)         # logical vector
+which(is.na(v2))  # indexes of NA
+
+
+################################ NaN ################################
+# NaN is often the result of non-valid calculations
+v3 <- sqrt(c(-5, -2, 0, 2))
+v3
+
+# is.nan() detects NaN
+is.nan(v1)
+which(is.nan(v1))
+
+
+################################ NULL ###############################
+# NULL is often the result of expressions whose result is not defined
+v4 <- c()
+v4
+
+# detect NULL
+is.null(v4)
+
+
+################################ Inf ################################
+# Inf stands for Infinum (infinite), -Inf for minus infinite
+v5 <- c(Inf, 2, 3, -Inf)
+v5
+
+# detect Inf, -Inf
+is.infinite(v5)
+# or:
+is.finite(v5)
+
+
 ############################# Functions #############################
 
 moments <- function(x, n = 2) {
   x ^ n
 }
 
+# position matching
 moments(3)
+moments(3, 2)
+# name matching
 moments(n = 2, 3)
 moments(2, x = 3)
 moments(n = 2, x = 3)
