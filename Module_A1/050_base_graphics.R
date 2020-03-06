4966d4486c979bef4c7f7a0a9ccad6f09debf541
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-03-06 12:37:43 +0100

    end of base R graphics lesson.

--- Module_A1/050_base_graphics.R
+++ Module_A1/050_base_graphics.R
@@ -55,3 +55,61 @@ barplot(table(Prestige$type))
 plot(Prestige$type)
 methods(plot)
 
+# rel. Haeufigkeit
+barplot(prop.table(table(Prestige$type)))
+# oder:
+barplot(table(Prestige$type) / nrow(Prestige))
+
+# Scatterplot
+# Formelschreibweise in R:
+# (Erster Variablenname ~ Zweiter Variablenname, data = DataSource)
+plot(prestige ~ income, data = Prestige)
+# mit roten Punkten
+plot(prestige ~ income, data = Prestige,
+     col = "red", pch = 20)
+
+# diese print character gibt es:
+plot(1:30, rep(1, 30), pch = 1:30)
+
+# alle bivariaten Vergleiche mit pairs()
+pairs(Prestige)
+pairs(Prestige[, c(4, 1:3, 6)])
+
+
+# Mosaicplot
+# anderer Datensatz
+# wir benoetigen mehr als eine kategorische (factor) Variable
+data(Cars93, package = "MASS")
+library(dplyr)
+Cars93 %>% 
+  select(DriveTrain, Origin, Type) %>% 
+  select(DriveTrain, Origin, Type) %>% 
+  summarise(n = n())
+
+mosaicplot(table(Cars93[, c("Type", "DriveTrain", "Origin")]),
+           main = "")
+
+# oder mit package 'vcd'
+library(vcd)
+mosaic(table(Cars93[, c("Type", "DriveTrain", "Origin")]),
+           main = "")
+
+
+# Korrelation
+cor(Prestige$education, Prestige$prestige) # default: method = "pearson"
+
+# robust:
+#install.packages("robustbase")
+library(robustbase)
+
+covMcd(Prestige[, c("education", "prestige")],
+       cor = TRUE)$cor
+# Korrelations-Matrix
+# nach Pearson
+cor(Prestige %>% select(-type))  # remove type (not numeric!)
+
+# robust
+covMcd(Prestige %>%  select(-type), cor = TRUE)$cor
+
+# Spearman-Korrelation (normalerweise nur auf ordinalen Daten)
+cor(Prestige %>% select(-type), method = "spearman")
