2881745f76d2cdf87d1ebbb1dea473677624a481
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-05-14 20:50:18 +0200

    prepared leistungsnachweis B1.

---
+++ Leistungsnachweis_B1/Leistungsnachweis_B1.R
@@ -0,0 +1,10 @@
+
+# data path
+data.path <- "/Users/schmis12/wrk/studio/ZHAW_CAS_Data_Analysis/Leistungsnachweis_B1/data/"
+
+library(readr)
+# xls
+df <- read_xls(paste0(data.path, "Alpenquerender_Gueterverkehr_Schiene.xls"))
+
+# dat
+df <- read.table(paste0(data.path, "softdrink.dat"), header = TRUE)
