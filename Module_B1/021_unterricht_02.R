49d4c3325cf99fcfdfafd450dd358c2b42711fa8
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-05-14 17:59:38 +0200

    module B1 lesson 2 reviewed.

--- Module_B1/021_unterricht_02.R
+++ Module_B1/021_unterricht_02.R
@@ -10,14 +10,17 @@
 data.path <- "/Users/schmis12/wrk/studio/ZHAW_CAS_Data_Analysis/Module_B1/data/"
 ga <- read.table(paste0(data.path, "Softdrink.dat"), header = TRUE)
 
-# Modell
+
+# Modell, Koeffizienten, Regressionsgerade
 fit.ga <- lm(Zeit ~ Menge, data = ga)
-# Koeffizienten
+
 coef(fit.ga)
-# Plot mit Regressiongeraden
-plot(Zeit ~ Menge, data = ga)
+
+plot(Zeit ~ Menge, data = ga, pch = 16)
 abline(fit.ga, col = 'red')
 
+
+# Schaetzen von alpha und beta (Simulation)
 # Wir simulieren mehrere Datensets von folgendem Modell:
 #       Yi = 4 − 2xi + Ei mit σ = 0.5
 # und schätzen dann mittels Regression jeweils α und β.
@@ -35,4 +38,93 @@ for(i in 1:nsim){
 #par(mfrow=c(1,2))
 hist(result[, 1], main = "Schaetzung fuer alpha")
 hist(result[, 2], main = "Schaetzung fuer beta")
+
+### Beziehung zwischen x und y
+# 1. Welcher Werte sind für die unbekannten Parameter α und β am plausibelsten?
+# 2. Welchen Erklärungsgehalt hat die Gerade?
+# 3. Hat die erklärende Variable einen signifikanten Einfluss auf die Zielgrösse?
+# 4. Welche Werte sind insgesamt plausibel?
+
+# 1. Welcher Werte sind für die unbekannten Parameter α und β am plausibelsten?
+#    Die Punktschätzungen aus dem KQ-Verfahren:
+fit.ga <- lm(Zeit ~ Menge, data = ga)
+coef(fit.ga)
+
+# 2. Welchen Erklärungsgehalt hat die Gerade?
+# Bestimmheitsmass R2
+# gute Werte: knapp unter 1 (z.B. 0.85)
+
+# 3. Hat die erklärende Variable einen signifikanten Einfluss auf die Zielgrösse?
+summary(fit.ga)
+# Coefficients:
+#                 Estimate  Std. Error  t value   Pr(>|t|)    
+# (Intercept)     3.321     1.371       2.422     0.0237 *  
+#   Menge         2.176     0.124       17.546    8.22e-15 ***
+#
+# Nullhypothese alpha=alpha0 muss verworfen werden (p-value < 0.05)
+# Nullhypothese beta=beta0 muss verworfen werden (p-value < 0.05)
+
+# 4. Welche Werte sind insgesamt plausibel?
+confint(fit.ga, level = 0.95)
+#               2.5 %       97.5 %
+# (Intercept)   0.4844979   6.157062
+# Menge         1.9195920   2.432741
+
+
+
+### Vorhersage
+# 1. Welcher y-Wert ist für ein gegebenes x am plausibelsten?
+# 2. Wie genau ist dieser vorhergesagte Wert für y?
+# 3. In welchem Bereich wird der Datenpunkt y liegen?
+
+fit.ga <- lm(Zeit ~ Menge, data = ga)
+x0 <- data.frame(Menge = 20)
+# Vorhersage:
+predict(fit.ga, newdata = x0)   # Data Frame uebergeben!
+#        1 
+# 46.84411 
+
+# Vertrauens Intervall fuer die erwartete Service-Zeit
+predict(fit.ga, newdata = x0, interval = "confidence", level = 0.95)
+#        fit      lwr     upr
+#1  46.84411 43.48112 50.2071
+#
+# Die erwartete Service-Zeit liegt zwischen 43.48 und 50.20 min.
+
+# Prognose Intervall fuer y von x0
+predict(fit.ga, newdata = x0, interval = "prediction", level = 0.95)
+#        fit      lwr      upr
+#1  46.84411 37.56348 56.12474
+
+
+# Darstellung von Vertrauens- und Prognose-Band
+datanew <- data.frame(Menge = seq(0, 35,by = 1))
+pred1 <- predict(fit.ga, newdata = datanew, interval = "confidence")
+pred2 <- predict(fit.ga, newdata = datanew, interval = "prediction")
+plot(ga$Menge, ga$Zeit); abline(fit.ga)
+lines(datanew$Menge, pred1[,2], col = "blue")
+lines(datanew$Menge, pred1[,3], col = "blue")
+lines(datanew$Menge, pred2[,2], col = "red")
+lines(datanew$Menge, pred2[,3], col = "red")
+legend("topleft", legend=c("Vertrauensband", "Prognoseband"), col=c("blue", "red"), lty=c(1, 1), cex=0.8)
+
+# Zusammenfassum zum Modell
+summary(fit.ga)
+# Call:
+#   lm(formula = Zeit ~ Menge, data = ga)
+# 
+# Residuals:
+#     Min      1Q  Median      3Q     Max 
+# -7.5811 -1.8739 -0.3493  2.1807 10.6342 
+# 
+# Coefficients:
+#               Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)      3.321      1.371   2.422   0.0237 *  
+#   Menge          2.176      0.124  17.546 8.22e-15 ***
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 4.181 on 23 degrees of freedom
+# Multiple R-squared:  0.9305,	Adjusted R-squared:  0.9275 
+# F-statistic: 307.8 on 1 and 23 DF,  p-value: 8.22e-15
 
--- Module_B1/024_arbeitsblatt_02.R
+++ Module_B1/024_arbeitsblatt_02.R
@@ -1,42 +1,154 @@
+data.path <- "/Users/schmis12/wrk/studio/ZHAW_CAS_Data_Analysis/Module_B1/data/"
 
-### Aufgabe 1 -----------------------------------------------------------------
-uhren <- read.table("./Module_B1/data/AntikeUhren.dat", header = TRUE)
 
-### a)
+### Aufgabe 1: Antike Uhren-------------------------------------------
+au <- read.table(paste0(data.path, "AntikeUhren.dat"), header = TRUE)
+
+# (a)
 # Scatterplot von Preis ~ Alter
-plot(Preis ~ Alter, data = uhren)
+plot(Preis ~ Alter, data = au)
 
 # angepasste Gerade:
-fit.uhren <- lm(Preis ~ Alter, data = uhren)
-abline(fit.uhren, col = "red")
+fit.au <- lm(Preis ~ Alter, data = au)
+abline(fit.au, col = "red")
 
 # Koeffizienten:
-coef(fit.uhren)
+coef(fit.au)
 
 # geschätzte Geradengleichung:
 # Preis = -191.66 + 10.48 * Alter
 
 
-### b)
+# (b)
 
 ## (1) mit Hilfe von summary():
-summary(fit.uhren)
-# Ja, das Alter hat einen signifikanten Einfluss (5% Signifikanz-Niveau):
+summary(fit.au)
+# Ja, H0: beta=0 muss mit p-value < 0.05 verworfen werden.
 # p < 2.1e-06 ***
 
 
 ## (2) manuell:
-hat_beta <- coef(fit.uhren)[2]          # geschaetztes beta
-ssx <- sum((uhren$Alter - mean(uhren$Alter))^2)
-se_beta <- sqrt(summary(fit.uhren)$sigma^2 / ssx)
+hat_beta <- coef(fit.au)[2]          # geschaetztes beta
+ssx <- sum((au$Alter - mean(au$Alter))^2)
+se_beta <- sqrt(summary(fit.au)$sigma^2 / ssx)
 T <- hat_beta / se_beta
 T
 # to be continued...
 
 
-### c)
-confint(fit.uhren, level = 0.99)
+# (c)
+confint(fit.au, parm = "Alter", level = 0.99)
 # Ja, Preiserhoehungen zwischen $ 5.55 und $ 15.40 sind plausibel (1% Niveau)
 
 
-### d)
+# (d)
+# Geschaetztes beta 'Alter'
+coef(fit.au)[2]
+# Innerhalb eines Jahres ist mit einer Preiszunahme von $10.48 zu rechnen
+confint(fit.au, parm = "Alter", level = 0.95)
+# Plausible Preiszunahmen liegen zwischen $6.82 und $14.13.
+
+# (e)
+x0 <- data.frame(Alter=160)
+# Vertrauens-Intervall (Variabilitaet angepasster Werte)
+predict(fit.au, newdata = x0, interval = 'confidence', level = 0.95)
+#        fit     lwr      upr
+# 1 1484.998 1372.09 1597.905
+
+
+# Prognose-Intervall (Bereich zukuenftiger Datenpunkte)
+predict(fit.au, newdata = x0, interval = 'prediction', level = 0.95)
+#        fit      lwr      upr
+# 1 1484.998 916.0829 2053.912
+
+# Prognose-Intervall nuetzlicher, es steckt Bereich zukuenftiger Preise ab.
+# Das Prognose-Intervall ist für den unerfahrenen Käufer wichtiger, weil es
+# sinnvoller ist, auch die stochastische Variabilität des Beobachtungsfehlers
+# Ei zu berücksichtigen und nicht nur die Ungenauigkeit, die sich aus der 
+# Schätzung des Modells ergibt.
+
+
+
+### Aufgabe 2: Conconi Test -------------------------------------------
+load(paste0(data.path, "conconi.rda"))
+str(conconi)
+
+# (a)
+# Stellen sie die Daten in einem Scatterplot dar, passen sie mit dem Befehl
+# lm() die Regressionsgerade an und zeichnen sie diese ein.
+
+plot(puls ~ speed, data = conconi)
+fit.co <- lm(puls ~ speed, data = conconi)
+abline(fit.co, col = 'red')
+
+# (b)
+# Zu welchem Prozentanteil lassen sich die Schwankungen in den Pulswerten durch
+# die Zunahme der Geschwindigkeit erklären?
+summary(fit.co)
+# Multiple R-squared:  0.9861
+#
+# Zu 98.61% werden die Schwankungen im Puls durch die Geschwindigkeitszunahme erklaert.
+
+
+# (c)
+# Mit welcher Pulsfrequenz muss der Läufer rechnen, wenn er mit 10km/h
+# unterwegs ist? Geben Sie ein 95% Prognoseintervall an.
+x0 <- data.frame(speed=10)
+predict(fit.co, newdata = x0, interval = 'prediction', level = 0.95)
+#        fit      lwr      upr
+# 1 150.6807 145.7308 155.6306
+# Er muss mit einem Puls von 150 rechnen.
+# Plausible Werte liegen zwischen 145 und 155 (95% Niveau)
+
+# (d)
+# Geben Sie an, wie hoch der Ruhepuls (d.h. keine Vorwärtsbewegung)
+# geschätzt wird. In welchem 95% Intervall würden Sie den entsprechenden
+# Messpunkt erwarten.
+coef(fit.co)[1]
+# (Intercept) 
+# 86.61053 
+confint(fit.co)
+#                 2.5 %    97.5 %
+# (Intercept) 81.257578 91.963475
+# speed        6.018418  6.795617
+# oder:
+predict(fit.co, newdata = data.frame(speed=0), interval = 'prediction', level = 0.95)
+#        fit      lwr      upr
+# 1 86.61053 79.52724 93.69381
+
+# (e)
+# Um wie viel nimmt der Puls im Schnitt zu, wenn die Geschwindigkeit um 1 km/h
+# erhöht wird? Welche anderen Werte sind für die Pulszunahme ebenfalls plausibel?
+coef(fit.co)[2]
+#    speed 
+# 6.407018 
+# Im Schnitt nimmt der Puls um 6.4 Schlaege zu
+confint(fit.co)
+#                 2.5 %    97.5 %
+# (Intercept) 81.257578 91.963475
+# speed        6.018418  6.795617
+# Plausibel sind Zunahmen zwischen 6.0 und 6.8 Schlaegen / min
+
+# (f)
+# Im File conconi2.rda stehen Ihnen die Daten eines zweiten Läufers zur
+# Verfügung. Wessen Puls steigt bei Geschwindigkeitserhöhung langsamer an?
+# Können sie eine Aussage treffen, ob zwischen den beiden ein auf 5% 
+# signifikanter Unterschied besteht? Lässt sich ableiten, wer der besser
+# trainierte Läufer ist?
+load(paste0(data.path, "conconi2.rda"))
+str(conconi2)
+plot(puls ~ speed, data = conconi2)
+fit.co2 <- lm(puls ~ speed, data = conconi2)
+abline(fit.co2, col = 'blue')
+coef(fit.co2)
+coef(fit.co)
+# beim 2. Laeufer steigt der Puls nur um 4.09,
+# beim 1. Laeufer um 6.40 pro Geschwindigkeitszunahme um 1 km/h.
+confint(fit.co2, parm = 'speed', level = 0.95)
+#            2.5 %   97.5 %
+#   speed 3.883739 4.302727
+confint(fit.co, parm = 'speed', level = 0.95)
+#            2.5 %   97.5 %
+#   speed 6.018418 6.795617
+# Der langsamere Pulsanstieg deutet darauf hin, dass Laeufer 2 der
+# besser trainierte ist.
