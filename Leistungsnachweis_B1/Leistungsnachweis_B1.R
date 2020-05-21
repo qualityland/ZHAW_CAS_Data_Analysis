0a3cf42e725ec4287372f331796366d6f5e16b5d
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-05-21 13:00:02 +0200

    renamed exam file.

--- Leistungsnachweis_B1/Leistungsnachweis_B1.R
+++ Leistungsnachweis_B1/Leistungsnachweis_B1.R
@@ -2,7 +2,7 @@
 # data path
 data.path <- "/Users/schmis12/wrk/studio/ZHAW_CAS_Data_Analysis/Leistungsnachweis_B1/data/"
 
-library(readr)
+library(readxl)
 # xls
 df <- read_xls(paste0(data.path, "Alpenquerender_Gueterverkehr_Schiene.xls"))
 
