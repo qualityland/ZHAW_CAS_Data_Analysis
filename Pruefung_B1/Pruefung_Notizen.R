551395c0e788c04967300167c277ecc097c9d2a2
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-05-27 13:20:55 +0200

    data file added.

--- Pruefung_B1/Pruefung_Notizen.R
+++ Pruefung_B1/Pruefung_Notizen.R
@@ -11,14 +11,33 @@ data.path <- "/Users/schmis12/wrk/studio/ZHAW_CAS_Data_Analysis/Pruefung_B1/data
 #df <- read_xls(paste0(data.path, "Alpenquerender_Gueterverkehr_Schiene.xls"))
 
 # csv
-highway <- read.csv(paste0(data.path, 'highway.csv'))
-
+#highway <- read.csv(paste0(data.path, 'highway.csv'))
+salary <- read.csv(paste0(data.path, "salary.csv"), header=TRUE)
 # dat
-windmill <- read.table(paste0(data.path, "Windmuehle.dat"), header = TRUE)
+#windmill <- read.table(paste0(data.path, "Windmuehle.dat"), header = TRUE)
 
 ###############################################################################
 #############################     Scatterplot     #############################
 ###############################################################################
+plot(
+  y ~ experience,
+  data = salary,
+  col = education,
+  pch = education,
+  xlim = c(0, 40),
+  ylim = c(10000, 150000),
+  xlab = "Berufserfahrung",
+  ylab = "Jahreslohn"
+)
+legend(
+  "topleft",
+  c("Berufslehre", "Maturität", "Hochschulabschluss"),
+  col = 1:3,
+  pch = 1:3
+)
+
+
+
 
 par(mfrow = c(1, 1))
 plot(Strom ~ Windgeschwindigkeit, data = windmill,
@@ -27,9 +46,13 @@ plot(Strom ~ Windgeschwindigkeit, data = windmill,
      xlab = "Windgeschwindigkeit [km/h]")
 
 
+
+
 ###############################################################################
 ######################     Model & Regressionsgerade     ######################
 ###############################################################################
+
+
 
 # Modell
 fit.wm1 <- lm(Strom ~ Windgeschwindigkeit, data = windmill)
