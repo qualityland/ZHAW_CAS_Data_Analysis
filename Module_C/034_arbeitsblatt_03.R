eea1066dbed5e6090ce64ea366b12c7e983018e9
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-06-17 12:31:46 +0200

    module C lesson 3 started.

---
+++ Module_C/034_arbeitsblatt_03.R
@@ -0,0 +1,43 @@
+# Aufgabe 1 - 
+
+data.path <- '/Users/schmis12/wrk/studio/ZHAW_CAS_Data_Analysis/Module_C/data/'
+load(paste0(data.path, 'mnist_2k.RData'))
+
+#cl <- kmeans(x[, -1], centers=10)
+
+#plot(x, col = cl$cluster, las=1)
+
+
+#library(cluster)
+#plot(silhouette(x=cl$cluster, dist=dist(x)))
+
+set.seed(79)
+label <- x[, 1]
+res_cl <- kmeans(x[, -1], centers=10)
+res_cl$tot.withinss
+library(Rtsne)
+res_tSNE <- Rtsne(x[, -1], perplexity = 20)
+df <- as.data.frame(res_tSNE$Y)
+names(df) <- c('tsne1', 'tsne2')
+df$label <- as.factor(x$label)
+df$cluster <- as.factor(res_cl$cluster)
+
+library(ggplot2)
+ggplot(data=df, aes(x = tsne1, y = tsne2, col=cluster, label=label))  +
+  geom_text(size=3) +
+  ggtitle("2D t-SNE Vizualizierung mit k-menas mit 10 Clustern")
+
+
+
+# Aufgabe 2 - 
+
+data.path <- '/Users/schmis12/wrk/studio/ZHAW_CAS_Data_Analysis/Module_C/data/'
+load(paste0(data.path, 'abst.RData'))
+str(abst)
+
+abst.km <- kmeans(abst)
+library(factoextra)
+fviz_nbclust(abst, kmeans, method="wss")
+
+
+
