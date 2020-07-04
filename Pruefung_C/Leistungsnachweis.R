86d3a7bc1d9d27d63814663db0d948221069df28
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-07-04 18:09:50 +0200

    dataset added.

---
+++ Pruefung_C/Leistungsnachweis.R
@@ -0,0 +1,5 @@
+# UCI Heart failure clinical records Data Set
+# URL: https://archive.ics.uci.edu/ml/datasets/Heart+failure+clinical+records
+# Dataset URL: https://archive.ics.uci.edu/ml/machine-learning-databases/00519/heart_failure_clinical_records_dataset.csv
+
+df <- read.csv('https://archive.ics.uci.edu/ml/machine-learning-databases/00519/heart_failure_clinical_records_dataset.csv')
